3b39b8f1fe504babb38fa5368594663e33603dc5
Omar Mirza <omirza@gmail.com>
2023-11-14 20:19:02 -0500

    Configured index.js

--- client/index.js
+++ client/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import App from './components/App.jsx';
-import store from './components/reducers/index.js';
+import { createRoot } from 'react-dom/client';
+import { store } from './store.js';
+import App from './App.jsx';
+import './style.css';
 
-// store={store}
 const root = createRoot(document.getElementById('root'));
-
 root.render(
     <Provider store={store}>
         <App />
