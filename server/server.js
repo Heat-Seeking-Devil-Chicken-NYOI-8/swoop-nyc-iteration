80293c95e78dbf17f17ba6ba3f4382ac5890ece9
MXW9699 <mitchell.wen@hotmail.com>
2023-11-15 17:20:20 -0500

    got google maps to render. can add markers and change center point my inputing zipcode

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const path = require('path');
 const db = require('./model.js');
-const controller = require('./controller.js');
+
+const googleMapsRouter = require('./Routers/googleMapsRouter.js');
+//const listingsRouter = require('./Routers/listingRouter.js');
 
 // parse JSON from incoming requests
 app.use(express.json());
@@ -10,42 +13,33 @@ app.use(express.json());
 // handle requests from static files
 app.use('/dist', express.static(path.join(__dirname, '../dist')));
 
-// handle API calls
-app.get('/api/getLatLng', controller.getLatLng, (req, res) => {
-    return res.status(200).send(res.locals.data)
-})
-
-app.get('/api/getListings', controller.getListings, (req, res) => {
-    return res.status(200).send(res.locals.data)
-})
-
-app.post('/api/addListing', controller.getData, (req, res) => {
-    return res.status(200).send(res.locals.data)
-})
-
 // route handler to respond with main app
 app.get('/', (req, res) => {
-    return res.status(200).sendFile(path.join(__dirname, '../public/index.html'));
+  return res.status(200).sendFile(path.join(__dirname, '../public/index.html'));
 });
+
+// handle API calls
+app.use('/api', googleMapsRouter);
+// app.use('/listings', listingsRouter);
 
 // catch-all route handler for any requests to an unknown route
 app.get('*', (req, res) => {
-    return res.sendStatus(404);
+  return res.sendStatus(404);
 });
 
 // global error handling middleware
 app.use((err, req, res, next) => {
-    const defaultErr = {
-        log: 'Express error handler caught unknown middleware error',
-        status: 500,
-        message: { err: 'An error occurred' },
-    };
-    const errorObj = Object.assign({}, defaultErr, err);
-    console.log(errorObj.log);
-    return res.status(errorObj.status).json(errorObj.message);
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
 });
 
 // initialize port listening
 app.listen(PORT, () => {
-    console.log(`Server listening on port: ${PORT}...`);
+  console.log(`Server listening on port: ${PORT}...`);
 });
--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,80 @@
-import React from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-import { Box, BottomNavigation, BottomNavigationAction } from "@mui/material";
-import AddAPhotoIcon from "@mui/icons-material/AddAPhoto";
-import ViewListIcon from "@mui/icons-material/ViewList";
-import PlaceIcon from "@mui/icons-material/Place";
-import Browse from "./components/Browse";
-import GetLocation from "./components/GetLocation";
-import Map from "./components/Map";
-import PreviewListing from "./components/PreviewListing";
-import Upload from "./components/Upload";
-import ViewListing from "./components/ViewListing";
-import { setNavPosition } from "./mainSlice";
+import React, { useEffect, useState } from 'react';
+import { Loader } from '@googlemaps/js-api-loader';
 
-export default function App() {
-  const dispatch = useDispatch();
-  const state = useSelector((state) => state.main);
+const App = () => {
+  /*************************STATES******************************************* */
+  const [map, setMap] = useState(null); // State to hold the map instance
+  const [zip, setZip] = useState('');
 
-  // if (!state.location)
-  //   return (
-  //     <Box>
-  //       <GetLocation />
-  //     </Box>
-  //   );
-  // else
+  /***************************USE EFFECT***************************************** */
+  //Set-up initial map of first render
+  useEffect(() => {
+    const loader = new Loader({
+      apiKey: 'AIzaSyADQU5Oic0aAZjytCZzVbo8MZOQSgNPqA4',
+      version: 'weekly',
+    });
+    // Load the Google Maps API library
+    loader.importLibrary('core').then(() => {
+      // Initialize the map on a DOM element with id of 'map' that is created on first render
+      const newMap = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 2,
+      });
+      //store the map as a state so it can be referenced outside
+      setMap(newMap);
+    });
+  }, []);
+
+  /****************************HANDLER FUNCTIONS************************************ */
+  //Create a random marker on the map
+  function addMarker() {
+    const x = Math.random() * 180 - 90;
+    const y = Math.random() * 360 - 180;
+    const newMarker = new google.maps.Marker({
+      position: { lat: x, lng: y },
+      map: map,
+    });
+
+    newMarker.addListener('click', () => {
+      alert('i am here');
+    });
+  }
+
+  //Send zip to back end to recenter map
+  function zipCenter(e) {
+    e.preventDefault();
+    console.log(zip);
+    fetch('/api/setZip', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ zip: zip }),
+    })
+      //expected data to be {lon: , lat:}
+      .then((data) => data.json())
+      .then((data) => {
+        console.log(data);
+        map.setCenter(data);
+      })
+      .catch(() => console.log('could not set center'));
+  }
+
+  /***********************************RENDER COMPONENT************************************** */
   return (
-    <Router>
-      <Box sx={{ display: "flex", flexDirection: "column", height: "100vh" }}>
-        <Box sx={{ flexGrow: 1 }}>
-          <Routes>
-            <Route path="/" element={<Browse />} />
-            <Route path="/upload" element={<Upload />} />
-            <Route path="/map" element={<Map />} />
-            <Route path="/previewlisting" element={<PreviewListing />} />
-            <Route path="/viewlisting" element={<ViewListing />} />
-          </Routes>
-        </Box>
-        <BottomNavigation
-          showLabels
-          value={state.navPosition}
-          onChange={(event, newPosition) => {
-            dispatch(setNavPosition(newPosition));
-          }}
-          sx={{ width: "100%", position: "fixed", bottom: 0 }}
-        >
-          <BottomNavigationAction
-            label="Upload"
-            icon={<AddAPhotoIcon />}
-            component={Link}
-            to="/upload"
-          />
-          <BottomNavigationAction
-            label="Browse"
-            icon={<ViewListIcon />}
-            component={Link}
-            to="/"
-          />
-          <BottomNavigationAction
-            label="Map"
-            icon={<PlaceIcon />}
-            component={Link}
-            to="/map"
-          />
-        </BottomNavigation>
-      </Box>
-    </Router>
+    <div>
+      <div id="map" style={{ height: '50%', width: '100%' }}></div>
+      <button onClick={addMarker} style={{ border: '1px solid red' }}>
+        add
+      </button>
+      <form name="myForm" onSubmit={(e) => zipCenter(e)} method="POST">
+        <input
+          type="text"
+          name="zip"
+          onChange={(e) => setZip(e.target.value)}
+        />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
   );
-}
+};
+
+export default App;
