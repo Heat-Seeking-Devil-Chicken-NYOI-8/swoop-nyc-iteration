431ebc2dc289410814ac61784fa6dae98e2a8321
Omar Mirza <omirza@gmail.com>
2023-11-16 13:52:01 -0500

    Working upload w/gps scraping and url

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -1,35 +1,40 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { setNavPosition } from "../mainSlice";
+import { useNavigate } from "react-router-dom";
+import { setNavPosition, savePhoto } from "../mainSlice";
 import { Box, Typography, Button, Icon } from "@mui/material";
+import TextField from "@mui/material/TextField";
 import { Upgrade } from "@mui/icons-material";
 import { createClient } from "@supabase/supabase-js";
+import exifr from "exifr";
 
 export default function Upload() {
   const state = useSelector((state) => state.main);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-  // const supabaseUrl = "https://iqmxeqilgrwqfrwxzqfz.supabase.co";
-  // const supabaseKey = process.env.REACT_APP_SUPABASE;
-  // const supabase = createClient(supabaseUrl, supabaseKey);
+  const supabaseUrl = "https://iqmxeqilgrwqfrwxzqfz.supabase.co";
+  const supabaseKey =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlxbXhlcWlsZ3J3cWZyd3h6cWZ6Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5OTk3ODcwNSwiZXhwIjoyMDE1NTU0NzA1fQ.mYd_pX1TRRmBIRGkpDCrBr_ezWF1RDSGVCp8rMlXYGo"; //process.env.REACT_APP_SUPABASE;
+  const supabase = createClient(supabaseUrl, supabaseKey);
 
-  // const url = async (image) => {
-  //   // Upload image to supabase and get the URL back
-  //   try {
-  //     const { data, error } = await supabase
-  //       .storage
-  //       .from('images')
-  //       .upload(image.name, image)
+  const handleFileChange = async (e) => {
+    const file = e.target.files[0];
+    const fileName = Math.trunc(10 ** 6 * Math.random()) + file.name;
 
-  //     const imageUrl = await supabase.storage
-  //       .from("images")
-  //       .getPublicUrl(image);
+    const { data, error } = await supabase.storage
+      .from("images")
+      .upload(fileName, file);
 
-  //     return imageUrl.data.publicUrl;
-  //   } catch (err) {
-  //     return console.log(err);
-  //   }
-  // };
+    const url = await supabase.storage.from("images").getPublicUrl(fileName)
+      .data.publicUrl;
+
+    let { latitude, longitude } = await exifr.gps(file);
+
+    dispatch(savePhoto({ url, latitude, longitude }));
+
+    navigate("/previewlisting");
+  };
 
   return (
     <Box display="flex" flexDirection="column" alignItems="center">
@@ -43,8 +48,8 @@ export default function Upload() {
           color: "#999",
         }}
       >
-        <Box paddingBottom={"5%"}>
-          <Upgrade sx={{ fontSize: "20vw", maxBlockSize: "130px" }}></Upgrade>
+        <Box>
+          <Upgrade sx={{ fontSize: "20vw", maxBlockSize: "130px" }} />
           <br />
           <Typography
             variant="body2"
@@ -56,6 +61,14 @@ export default function Upload() {
             <br />
             Photo
           </Typography>
+          <br />
+          <input
+            type="file"
+            id="file"
+            name="file"
+            accept="image/png, image/jpeg"
+            onChange={handleFileChange}
+          ></input>
         </Box>
       </Button>
     </Box>
