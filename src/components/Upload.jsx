8d5bca880f60faf04dec7c380947e2d89359a172
Darryl <hjdjoo@gmail.com>
2023-11-16 13:08:40 -0500

    Upload page now has a functioning file upload that looks real nice

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setNavPosition } from "../mainSlice";
-import { Box, Typography, Button, Icon } from "@mui/material";
+import { Box, Typography, Button, Icon, Input } from "@mui/material";
 import { Upgrade } from "@mui/icons-material"
 
 export default function Upload() {
   const state = useSelector((state) => state.main);
   const dispatch = useDispatch();
+
+
 
   return (
     <Box
@@ -14,15 +16,18 @@ export default function Upload() {
       flexDirection="column"
       alignItems="center"
     >
-      <Button sx={{
+
+      <Button component="label" sx={{
         border: "1px dotted darkGray",
-        minHeight: "82vh", minWidth: "83vw", marginTop: "3vh", marginX: "5vw", marginBottom: "6vh"
+        minHeight: "82vh", minWidth: "83vw", marginTop: "3vh", marginX: "5vw", marginBottom: "6vh", zIndex: "0"
       }}>
-        <Box paddingBottom={"5%"}>
-          <Upgrade sx={{ fontSize: "20vw", maxBlockSize: "130px", color: "#EEE" }} ></Upgrade>
+        <Input type="file" id="uploadInput" style={{ width: "1", height: "1", marginTop: "3vh", marginX: "5vw", marginBottom: "6vh", visibility: "hidden", zIndex: "2", position: "absolute" }} />
+        <Box paddingBottom={"5%"} sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+          <Upgrade sx={{ fontSize: "20vw", maxBlockSize: "130px", color: "#EEE", justifySelf: "center" }} ></Upgrade>
           <br />
           <Typography color="#EEE" variant="body2" textAlign="center" fontSize={"8vw"} maxBlockSize="180px">Click to Upload Photo</Typography>
         </Box>
+
       </Button >
     </Box >
 
